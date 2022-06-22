7b066651c502759e5e269d8d67605dc79c847129
Louis Feather <106537794+GooseLF@users.noreply.github.com>
2022-06-22 12:21:50 +0100

    feat: Added CI and Release Builds for Debian, Windows and Mac (#12)

---
+++ platform/mac_osx/build.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# Install unixodbc and freetds
+brew install unixodbc freetds jq
+
+unixodbc_ver=$((brew info unixodbc --json) | jq -r '.[0].linked_keg')
+
+#Set flags for unixodbc
+export LDFLAGS="-L/opt/homebrew/Cellar/unixodbc/$unixodbc_ver/lib"
+export CPPFLAGS="-I/opt/homebrew/Cellar/unixodbc/$unixodbc_ver/include"
+
+# Install the required Python libraries, including `pyinstaller`
+python3 -m pip install --upgrade pip
+python3 -m pip install \
+    --quiet \
+    --requirement requirements/build.txt \
+    --user
+
+# Keep things clean: destroy the `dist/macosx/` directory if it exists.
+rm -rf dist/macosx/
+
+# Create the executable!
+python3 -m PyInstaller ./src/sql-deployment-tools.py \
+    --clean \
+    --noconfirm \
+    --log-level WARN \
+    --onefile \
+    --console \
+    --add-data "./src/sql:sql" \
+    --distpath="dist/macosx"
+
+# Run the app to check it has at least built!
+./dist/macosx/sql-deployment-tools --help
