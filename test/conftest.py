d1a6922e66070e86f32e57246a14fcb4d8cc2e6d
Louis Feather <louis.feather@thedatashed.co.uk>
2022-06-24 10:43:03 +0100

    feat: Adding more unit tests for SsisDeployment

--- test/conftest.py
+++ test/conftest.py
@@ -1,16 +1,35 @@
-import os
-import sys
-
-import pytest
-
-_ROOT = os.path.dirname(__file__)
-VALID_CONFIG = "config_templates/valid_config.toml"
-INVALID_CONFIG = "config_templates/invalid_config.toml"
-
-
-def get_config_text(path):
-    if not path:
-        raise Exception("path cannot be null or empty.")
-
-    with open(os.path.join(_ROOT, path)) as file:
-        return file.read()
+TEST_CONFIG = {
+    "project": "Bob Ross",
+    "folder": "def",
+    "environment": "default",
+    "parameters": [
+        {
+            "name": "Season 1 Episode 1",
+            "value": "A walk in the woods",
+            "sensitive": False,
+        },
+        {"name": "Season 1 Episode 2", "value": "Mt. McKinley", "sensitive": True},
+    ],
+    "job": {
+        "name": "Season 1 Episode 3",
+        "description": "Ebony Sunset",
+        "enabled": True,
+        "notification_email_address": "Bob.Ross@thedatashed.co.uk",
+        "steps": [
+            {
+                "name": "Season 1 Episode 4",
+                "type": "SSIS",
+                "package": "WinterMist.dtsx",
+            },
+            {
+                "name": "Season 1 Episode 5",
+                "type": "SSIS",
+                "package": "QuietStream.dtsx",
+            },
+        ],
+        "schedules": [
+            {"name": "Winter Moon", "every_n_minutes": 30},
+            {"name": "Autumn Mountain", "every_n_minutes": 1440},
+        ],
+    },
+}
